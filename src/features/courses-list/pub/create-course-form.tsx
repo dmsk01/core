21bfe3b8c011a5f99747d1616c2d14b6970e2f59
Dmitry Petrov <petrov3.14@mail.ru>
2024-06-25 13:37:35 +0300

    delete env file, add course create form adn courses list

---
+++ src/features/courses-list/pub/create-course-form.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/shared/ui/form";
+import { Input } from "@/shared/ui/input";
+import { Textarea } from "@/shared/ui/textarea";
+import { startTransition, useTransition } from "react";
+import { createCourseAction } from "../actions";
+import { Button } from "@/shared/ui/button";
+
+const createCourseFormSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+});
+
+export function CreateCourseForm({
+  revalidatePagePath,
+}: {
+  revalidatePagePath: string;
+}) {
+  const [isCreateTransition, startCreateTransition] = useTransition();
+  const form = useForm<z.infer<typeof createCourseFormSchema>>({
+    resolver: zodResolver(createCourseFormSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof createCourseFormSchema>) {
+    startTransition(async () => {
+      createCourseAction(values, revalidatePagePath);
+    });
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-[300px] mb-10"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Course name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Descripton..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isCreateTransition}>
+          Add course
+        </Button>
+      </form>
+    </Form>
+  );
+}
